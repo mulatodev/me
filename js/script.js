549a7a805b0eea8e52241d858bffbcd3dbb0da7e
mulatodev <ganaranjo@gmail.com>
2023-01-03 15:41:48 -0500

    Reestructuración del sitio, nuevo css y script

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/*
+    add event listener on multiple elements
+*/
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+/*
+    navbar toggle for mobile
+*/
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
